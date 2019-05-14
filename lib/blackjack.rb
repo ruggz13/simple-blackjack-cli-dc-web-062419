04811301bed8002c73b00a9e1b6c8c42cb86298d
Nick Ruggieri <nicolasruggieri7@gmail.com>
2019-05-14 18:34:54 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -6,8 +6,9 @@ def deal_card
   # code #deal_card here
 end
 
-def display_card_total
+def display_card_total(card_total)
   # code #display_card_total here
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
@@ -41,4 +42,3 @@ end
 def runner
   # code runner here
 end
-    
